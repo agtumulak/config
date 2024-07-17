c8ff0d76fef651b70e0a4070464f3fdd7b080a97
Aaron G. Tumulak <atumulak@lanl.gov>
2024-07-17 09:32:32 -0600

    Add gen.nvim with Ollama

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -340,6 +340,7 @@ require("lazy").setup({
                 build =
                 "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
             },
+            "nvim-telescope/telescope-ui-select.nvim",
         },
         keys = {
             { "<leader>;",  "<cmd>Telescope cmdline<cr>",                                               desc = "Cmdline" },
@@ -349,29 +350,31 @@ require("lazy").setup({
             { "<leader>h",  "<cmd>Telescope help_tags<cr>",                                             desc = "Search help tags" },
             { "<leader>o",  function() require("telescope.builtin").jumplist { show_line = false } end, desc = "Search jumplist" },
         },
-        opts = {
-            defaults = {
-                layout_strategy = "vertical",
-            },
-            extensions = {
-                file_browser = {
-                    mappings = {
-                        n = {
-                            ["h"] = function(prompt_bufnr, bypass)
-                                require("telescope").extensions.file_browser.actions
-                                    .goto_parent_dir(prompt_bufnr, bypass)
-                            end,
-                            ["l"] = function(prompt_bufnr) require("telescope.actions").select_default(prompt_bufnr) end,
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup {
+                defaults = {
+                    layout_strategy = "vertical",
+                },
+                extensions = {
+                    file_browser = {
+                        mappings = {
+                            n = {
+                                ["h"] = function(prompt_bufnr, bypass)
+                                    require("telescope").extensions.file_browser.actions
+                                        .goto_parent_dir(prompt_bufnr, bypass)
+                                end,
+                                ["l"] = function(prompt_bufnr) require("telescope.actions").select_default(prompt_bufnr) end,
+                            },
                         },
                     },
+                    ["ui-select"] = { require("telescope.themes").get_dropdown {} }
                 },
-            },
-        },
-        config = function(_, opts)
-            local telescope = require("telescope")
-            telescope.setup(opts)
+            }
             telescope.load_extension("fzf")
             telescope.load_extension("file_browser")
+            -- for gen.nvim selections
+            telescope.load_extension("ui-select")
             telescope.load_extension("cmdline")
             vim.cmd "autocmd User TelescopePreviewerLoaded setlocal number"
         end,
@@ -492,5 +495,44 @@ require("lazy").setup({
     -- https://github.com/g2boojum/vim-mcnp
     {
         "g2boojum/vim-mcnp",
+    },
+    -- https://github.com/David-Kunz/gen.nvim?tab=readme-ov-file#install
+    -- https://ddw-confluence.lanl.gov/display/XCP3GROUP/LLM+AI+Resources
+    {
+        "David-Kunz/gen.nvim",
+        opts = {
+            model = "llama3.1:70b",
+            host = "lilgun.lanl.gov",
+            port = "60003",
+            display_mode = "split",
+            show_model = true,
+        },
+        keys = {
+            { "<leader>ai", ":Gen<CR>", {}, mode = { "n", "v" }, desc = "Provide prompt for generative AI" },
+        },
+        config = function(_, opts)
+            require("gen").setup(opts)
+            require("gen").prompts = {
+                ["Chat"] = {
+                    prompt = "$input",
+                },
+                ["Explain code"] = {
+                    prompt =
+                    "Explain the following code:\n```$filetype\n$text\n```\n\nBegin the explaination by restating the code in format ```$filetype\n...\n```\n\n$input",
+                },
+                ["Modify code"] = {
+                    prompt =
+                    "$input:\n```$filetype\n$text\n```\n\nOnly output the result in format ```$filetype\n...\n```",
+                    replace = true,
+                    extract = "```$filetype\n(.-)```",
+                },
+                ["Generate code"] = {
+                    prompt =
+                    "Output $filetype code in a single block with format ```$filetype\n...\n```\n\nwith prompt: $input",
+                    replace = true,
+                    extract = "```$filetype\n(.-)```",
+                },
+            }
+        end,
     },
 }, { ui = { border = "rounded" } })
