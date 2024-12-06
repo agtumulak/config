7906da7ed0226804ae33b733f8978a3151071ce4
Aaron G. Tumulak <atumulak@lanl.gov>
2024-12-06 00:55:25 -0700

    Remove cmdline load_extension
    Neovim 0.10.0+ is required. Warnings were being emitted at startup but
    `:Telescope cmdline` still works after removing call to load_extension.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -342,7 +342,6 @@ require("lazy").setup({
             telescope.setup(opts)
             telescope.load_extension("fzf")
             telescope.load_extension("file_browser")
-            telescope.load_extension("cmdline")
             telescope.load_extension("ui-select")
             vim.cmd "autocmd User TelescopePreviewerLoaded setlocal number"
         end,
