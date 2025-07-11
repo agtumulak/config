fb37972adbf282b83544e3b3e3957a63fe7a39ac
Aaron G. Tumulak <atumulak@lanl.gov>
2025-07-11 12:09:54 -0600

    Add sambanova

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -565,9 +565,9 @@ require("lazy").setup({
         config = true,
         opts = {
             strategies = {
-                chat = { adapter = "ollama", },
-                inline = { adapter = "ollama" },
-                cmd = { adapter = "ollama" },
+                chat = { adapter = "sambanova", },
+                inline = { adapter = "sambanova" },
+                cmd = { adapter = "sambanova" },
             },
             adapters = {
                 ollama = function()
@@ -579,6 +579,21 @@ require("lazy").setup({
                         schema = {
                             model = {
                                 default = "llama3.3:latest",
+                            },
+                        },
+                    })
+                end,
+                sambanova = function()
+                    return require("codecompanion.adapters").extend("openai_compatible", {
+                        name = "sambanova",
+                        formatted_name = "SambaNova",
+                        env = {
+                            url = "SAMBANOVA_URL",
+                            api_key = "SAMBANOVA_API_KEY",
+                        },
+                        schema = {
+                            model = {
+                                default = "sambanova/Meta-Llama-3.3-70B-Instruct",
                             },
                         },
                     })
